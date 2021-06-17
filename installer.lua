d7e45218fd9c877567e35791bad859813ae85502
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-06-17 16:19:41 -0500

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,11 +1,15 @@
 --[[
     Main branch
-    loadstring(http.get("https://raw.githubusercontent.com/raphtalia/ComputerCraft-scripts/main/installer.lua").readAll())()
+    loadstring(http.get("https://raw.githubusercontent.com/raphtalia/ComputerCraft-scripts/main/installer.lua").readAll())()()
+    OR
+    pastebin run QjPNA4d0
 
     Development branch
-    loadstring(http.get("https://raw.githubusercontent.com/raphtalia/ComputerCraft-scripts/development/installer.lua").readAll())()
+    loadstring(http.get("https://raw.githubusercontent.com/raphtalia/ComputerCraft-scripts/development/installer.lua").readAll())()()
+    OR
+    pastebin run PRPGTyxj
 ]]
 
 return function()
-    print("test")
+    error("test")
 end

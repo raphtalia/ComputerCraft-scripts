aee44f0ea9603e454b2a3a8eb8788073353123af
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-06-18 13:29:40 -0500

    tests

--- src/startup.lua
+++ src/startup.lua
@@ -1,2 +1,31 @@
-print("startup")
-print(shell.getRunningProgram())
+local ROOT_PATH = fs.getDir(shell.getRunningProgram())
+local SRC_PATH = ROOT_PATH.. "/.raphtalia/src"
+
+local LIBRARIES_PATH = SRC_PATH.. "/Libraries"
+local SERVICES_PATH = SRC_PATH.. "/Services"
+local HANDLERS_PATH = SRC_PATH.. "/Handlers"
+
+local Environment = _G
+
+local function require(path, env, ...)
+    if fs.isDir(path) then
+        return (loadfile(path.. "/init.lua", env) or loadfile(path.. "/main.lua", env))(...)
+    else
+        return (loadfile(path, env))(...)
+    end
+end
+
+for _,libraryName in ipairs(fs.list(LIBRARIES_PATH)) do
+    print("Loading library ".. libraryName)
+    require(("%s/%s"):format(LIBRARIES_PATH, libraryName), Environment)
+end
+
+for _,serviceName in ipairs(fs.list(SERVICES_PATH)) do
+    print("Loading service ".. serviceName)
+    require(("%s/%s"):format(SERVICES_PATH, serviceName), Environment)
+end
+
+for _,handlerName in ipairs(fs.list(HANDLERS_PATH)) do
+    print("Loading handler ".. handlerName)
+    require(("%s/%s"):format(HANDLERS_PATH, handlerName), Environment)
+end
